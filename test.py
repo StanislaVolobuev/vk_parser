d246cd8117e5c3860d1e318df681108d03567696
StanislaVolobuev <67743701+StanislaVolobuev@users.noreply.github.com>
2021-01-11 20:52:07 +0700

    Add files via upload

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import  vk
 import openpyxl
 
 
-my_id = 413569747
+my_id = 11132894
 def user_f(id):
     session = vk.Session(access_token="token")
     vk_api = vk.API(
@@ -18,7 +18,7 @@ def user_f(id):
     return user_fields
 
 
-p = user_f(413569747)
+p = user_f(11132894)
 list_keys = list(p[0].keys())
 print(list_keys, len(list_keys))
 for key in list(p[0].keys()):
@@ -26,36 +26,65 @@ for key in list(p[0].keys()):
 
 
 def data_change(p):
+    head = ['first_name', 'id', 'last_name', 'sex', 'screen_name', 'verified', 'friend_status', 'nickname', 'maiden_name', 'domain', 'bdate', 'city', 'country', 'timezone', 'has_photo', 'has_mobile', 'is_friend', 'can_post', 'can_see_audio', 'skype', 'wall_default', 'interests', 'books', 'tv', 'quotes', 'about', 'games', 'movies', 'activities', 'music', 'can_write_private_message', 'can_send_friend_request', 'mobile_phone', 'home_phone', 'site', 'status', 'exports', 'followers_count', 'is_favorite', 'is_hidden_from_feed', 'occupation', 'career', 'military', 'university', 'university_name', 'faculty', 'faculty_name', 'graduation', 'education_form', 'education_status', 'home_town', 'relation', 'personal', '#alcohol', '#inspired_by', '##langs', '#life_main', '#people_main', '#religion', '#smoking', 'universities', 'schools', 'relatives', 'counters', '#albums', '#audios', '#followers', '#friends', '#gifts', '#notes', '#online_friends', '#pages', '#photos', '#subscriptions', '#user_photos', '#videos', '#new_photo_tags', '#new_recognition_tags', '#clips_followers', '#groups']
     data = list()
     header = list()
+    print('gjhhkjjlijkjgghc', len(head))
+    num = 0
+    while num < 80:
+        if num < 11 or 12 < num < 36 or 36 < num <40:
+            data.append(p[0][head[num]])
+        if num == 11 or num == 12:
+            data.append(p[0][head[num]]['title'])
+        if num == 36:
+            pass
+        if num == 40:
+            data_str = ''
+            if len(p[0][head[num]]) > 0:
+                for key in p[0][head[num]].keys():
+                    data_str += ' '
+                    data_str += str(p[0][head[num]][key])
+            data.append(data_str)
+        if num == 41:
+
+            pass
+        num += 1
+    print('data', data)
+
+
+    '''
     for key in list(p[0].keys()):
         if key  in ['city', 'country']:
-            print('ok', p[0][key]['title'])
+            print('ok', key, p[0][key]['title'])
             # data[key] = p[0][key]['title']
             header.append(key)
             data.append(p[0][key]['title'])
-        elif key  in ['career', 'military', 'personal', 'universities', 'schools', 'relatives']:
-            print('ok', ' '.join(p[0][key]))
+        elif key  in ['interests', 'career', 'military', 'personal', 'universities', 'schools', 'relatives']:
+            print('ok', key, ' '.join(p[0][key]))
             # data[key] = ' '.join(p[0][key])
             header.append(key)
             data.append(' '.join(p[0][key]))
-        elif key == 'counters':
-            for i in list(p[0][key].keys()):
-                print('ok', p[0][key][i])
+        elif key in ['counters', 'personal']:
+            for i in p[0][key]:
+                print('ok', i, p[0][key][i])
                 # data[i] = p[0][key][i]
                 header.append(i)
                 data.append(p[0][key][i])
         else:
-            print('ok', p[0][key])
+            print('ok', key, p[0][key])
             header.append(key)
             data.append(p[0][key])
-    print(header)
-    print(data)
-        # data[key] = p[0][key]
+#    print(header)
+#    print(data)
 
+        # data[key] = p[0][key]
+'''
 
 
 data_use =data_change(p)
+
+
+
 
 def friends_list(use_id=my_id):
     session = vk.Session(access_token="token")
