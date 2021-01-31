e8e5c0f1a40eb17b8a7b15bc26a9eb6c3071951e
StanislaVolobuev <67743701+StanislaVolobuev@users.noreply.github.com>
2021-01-31 12:04:29 +0700

    Add files via upload

--- test_data.py
+++ test_data.py
@@ -7,18 +7,18 @@ token = "token"
 def API(token):
     session = vk.Session(access_token=token)
     vk_api = vk.API(
-        session,  v = '5.35' ,
+        session,  v = '5.126' ,
         lang = 'ru' ,
-        timeout = 10
+        timeout = 20
     )
     return vk_api
 
 
 def old(user):
-    born = 'fail'
-    avr=1980
+    born = 'не определено поиском'
+    avr=1960
     ageFromTo=[avr]
-    for i in range(1,40):
+    for i in range(1,60):
         ageFromTo.append(avr+i)
         ageFromTo.append(avr-i)
     vk_api = API(token)
@@ -31,6 +31,7 @@ def old(user):
     rn = 0
     flag = False
     for i in ageFromTo:
+        vk_api = API(token)
         while True:
             try:
                 ans=vk_api.users.search(q=fname+' '+lname,count=1000,birth_year=i)
@@ -40,52 +41,76 @@ def old(user):
                     time.sleep(1)
                     continue
 
-
-
-        # time.sleep(0.5)
-
         if ans['count'] > 0:
             for j in ans['items']:
                 if j['id'] == user:
-                    print('bingo', i, fname, j)
                     born = i
+                    print('bingo', born, fname, j)
                     flag=True
         if flag:
             break
-        print(i, user, ans)
+        print(born, i, user, ans)
     return born
 
 
 def user_field(id):
+    """
+
+    :param id:
+    :return:
+    """
+    time.sleep(1)
     vk_api = API(token)
     user_fields = vk_api.users.get(
         user_id=id,
         fields="deactivated, is_closed, can_access_closed, about, activities, bdate, books, can_post, can_see_audio, can_send_friend_request, can_write_private_message, career, city, connections, contacts, counters, country, domain, education, exports, followers_count, friend_status, games, has_mobile, has_photo, home_town, interests, is_favorite, is_friend, is_hidden_from_feed, lists,maiden_name, military, movies, music, nickname, occupation, personal, quotes, relatives, relation, schools, screen_name, site,   sex, status, timezone, trending, tv, universities, verified, wall_default"
     )
 
-
+    time.sleep(1)
     user_wall_own = vk_api.wall.get(
         user_id=11132894, filter = 'owner', count = "1")
+    time.sleep(1)
 
     user_wall_all = vk_api.wall.get(
         user_id=11132894, filter = 'all', count = "1")
     user_fields[0]['wall_own'] = user_wall_own['count']
     user_fields[0]['wall_all'] = user_wall_all['count']
-
+    id_for_born = id
+    born = 'не удалось определить год рождения'
     if 'bdate' in user_fields[0]:
         if len(user_fields[0]['bdate']) > 5:
             born = user_fields[0]['bdate'].split('.')[-1]
 
         else:
-            born = old(id)
-        print(born)
+            born = old(id_for_born)
+    else:
+        born = old(id_for_born)
+    print(born)
 
+    return user_fields, born
 
-
-    return user_fields
+head = [
+    'first_name', 'id', 'last_name', 'sex', 'screen_name', 'verified',
+    'friend_status', 'nickname', 'maiden_name', 'domain', 'bdate',
+    'city', 'country', 'timezone', 'has_photo', 'has_mobile',
+    'is_friend', 'can_post', 'can_see_audio', 'skype', 'wall_default',
+    'interests', 'books', 'tv', 'quotes', 'about', 'games', 'movies',
+    'activities', 'music', 'can_write_private_message',
+    'can_send_friend_request', 'mobile_phone', 'home_phone', 'site',
+    'status', 'exports', 'followers_count', 'is_favorite',
+    'is_hidden_from_feed', 'occupation', 'career', 'military',
+    'university', 'university_name', 'faculty', 'faculty_name',
+    'graduation', 'education_form', 'education_status', 'home_town',
+    'relation', 'personal', 'alcohol', 'inspired_by', 'langs',
+    'life_main', 'people_main', 'religion', 'smoking', 'universities',
+    'schools', 'relatives', 'counters', 'albums', 'audios', 'followers',
+    'friends', 'gifts', 'notes', 'online_friends', 'pages', 'photos',
+    'subscriptions', 'user_photos', 'videos', 'new_photo_tags',
+    'new_recognition_tags', 'clips_followers', 'groups', 'wall_own', 'wall_all'
+]
 p = user_field(id)
 
-def data_change(p):
+def data_change(p, head):
     '''
     функция подготовки данных для переноса в эксель
     используя список head (его значения - названия столбцов) находит соответствующую
@@ -94,28 +119,10 @@ def data_change(p):
     :return:head - список полей для обработки данных и внесения в эксель
             data - список содержащий данные в соответствии со списком head
     '''
-    head = [
-        'first_name', 'id', 'last_name', 'sex', 'screen_name', 'verified',
-        'friend_status', 'nickname', 'maiden_name', 'domain', 'bdate',
-        'city', 'country', 'timezone', 'has_photo', 'has_mobile',
-        'is_friend', 'can_post', 'can_see_audio', 'skype', 'wall_default',
-        'interests', 'books', 'tv', 'quotes', 'about', 'games', 'movies',
-        'activities', 'music', 'can_write_private_message',
-        'can_send_friend_request', 'mobile_phone', 'home_phone', 'site',
-        'status', 'exports', 'followers_count', 'is_favorite',
-        'is_hidden_from_feed', 'occupation', 'career', 'military',
-        'university', 'university_name', 'faculty', 'faculty_name',
-        'graduation', 'education_form', 'education_status', 'home_town',
-        'relation', 'personal', 'alcohol', 'inspired_by', 'langs',
-        'life_main', 'people_main', 'religion', 'smoking', 'universities',
-        'schools', 'relatives', 'counters', 'albums', 'audios', 'followers',
-        'friends', 'gifts', 'notes', 'online_friends', 'pages', 'photos',
-        'subscriptions', 'user_photos', 'videos', 'new_photo_tags',
-        'new_recognition_tags', 'clips_followers', 'groups', 'wall_own', 'wall_all'
-    ]
+
     data = list()
     header = list()
-    print('gjhhkjjlijkjgghc', len(head), head)
+    # print('gjhhkjjlijkjgghc', len(head), head)
     num = 0
     while num < 83:
         if num < 11 or 12 < num < 36 or 36 < num <40 or 42 < num < 52 or 79 < num < 82:
@@ -194,11 +201,11 @@ def data_change(p):
                 data.append('-')
 
         num += 1
-    print('data', data)
-    return data, head
+    # print('data', data)
+    return data
 
 
-test2 = data_change(p)
+# test2 = data_change(p)
 
 
 
@@ -210,10 +217,33 @@ def friends_list(use_id = id, token = token):
     )
     print('test3', user_friends)
     return user_friends
-
+'''
 test3 = friends_list()
 
 test4 = user_field(test3['items'][17])
-test5 = data_change(test4)
+test5 = data_change(test4, head)
 bd = old(test3['items'][17])
 # print(bd)
+print(p)
+'''
+def main(*args, **kwargs):
+    data = dict()
+    list_friends = friends_list(id, token)['items']
+    print(list_friends)
+    stats =[]
+    nonstats =[]
+    for i in list_friends:
+        data_rows =[]
+        user_row = user_field(i)
+        data_rows.append(user_row[1])
+        data_rows.append(data_change(user_row[0], head))
+        print('next')
+        print('/n', data_rows[0], data_rows[1][0], data_rows[1][1])
+
+        if data_rows[0] =='не определено поиском':
+            nonstats.append(data_rows[1][1])
+        else:
+            stats.append(data_rows)
+
+if __name__ == '__main__':
+    main()
