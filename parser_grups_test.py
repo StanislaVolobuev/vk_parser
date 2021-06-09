e652b3d85fab756100ffe12c880c1a825fd97241
Stanislav <67743701+StanislaVolobuev@users.noreply.github.com>
2021-06-09 23:46:45 +0700

    Add files via upload

--- parser_grups_test.py
+++ parser_grups_test.py
@@ -2,22 +2,23 @@ import vk
 import openpyxl
 import time, datetime
 
-id = 'peshkanavalnoga'   # по этому id осуществляется парсинг списка id из списка друзей
-token = "токен"
+id = 'navalniymem'   # по этому id осуществляется парсинг списка id из списка друзей
+token = "token"
                 # token ключ доступа, необходимо
                 # зарегестрировать на пользователя, в соответствии с id выше
 
 
 
 head = [
-    'first_name', 'id', 'last_name', 'sex', 'screen_name',
+    'id', 'first_name', 'last_name','bdate',  'sex', 'screen_name',
     'verified', 'friend_status', 'nickname', 'maiden_name', 'domain',
-    'bdate', 'city', 'country', 'timezone', 'has_photo',
-    'has_mobile', 'is_friend', 'can_post', 'can_see_audio', 'skype',
+    'city', 'country', 'timezone', 'has_photo',
+    'has_mobile', 'is_friend', 'can_post', 'can_see_audio',
+    'site', 'skype',
     'wall_default','interests', 'books', 'tv', 'quotes',
     'about', 'games', 'movies', 'activities', 'music',
     'can_write_private_message', 'can_send_friend_request', 'mobile_phone', 'home_phone',
-    'site', 'status', 'exports', 'followers_count', 'is_favorite',
+    'status', 'exports', 'followers_count', 'is_favorite',
     'is_hidden_from_feed', 'occupation', 'career', 'military', 'university',
     'university_name', 'faculty', 'faculty_name', 'graduation', 'education_form',
     'education_status', 'home_town', 'relation', 'personal', 'political',
@@ -30,9 +31,23 @@ head = [
     'facebook', 'twitter', 'livejournal', 'instagram'
 ] # Список ключей для обработки ответа сервера
 
-head_group = ['first_name', 'id', 'last_name', 'can_access_closed', 'is_closed', 'sex', 'photo_50', 'photo_100', 'online', 'domain', 'bdate', 'city', 'country', 'photo_200', 'photo_max', 'photo_200_orig', 'photo_400_orig', 'photo_max_orig', 'has_mobile', 'can_post', 'can_see_all_posts', 'can_see_audio', 'skype', 'can_write_private_message', 'mobile_phone', 'site', 'status', 'last_seen', 'common_count', 'home_phone', 'university', 'university_name', 'faculty', 'faculty_name', 'graduation', 'education_form', 'education_status', 'relation', 'universities', 'schools', 'relatives', 'twitter', 'deactivated', 'relation_partner', 'facebook', 'facebook_name', 'instagram', 'online_mobile', 'online_app', 'status_audio']
+head_group = ['first_name', 'id', 'last_name', 'can_access_closed',
+              'is_closed', 'sex', 'photo_50', 'photo_100',  'online',
+              'domain', 'bdate', 'city', 'country','photo_200',
+              'photo_max', 'photo_200_orig', 'photo_400_orig',
+              'photo_max_orig', 'has_mobile', 'can_post',
+              'can_see_all_posts', 'can_see_audio', 'skype',
+              'can_write_private_message', 'mobile_phone', 'site',
+              'status', 'platform', 'last_seen', 'common_count', 'home_phone',
+              'university', 'university_name', 'faculty', 'faculty_name',
+              'graduation', 'education_form', 'education_status',
+              'relation', 'universities', 'schools', 'relatives',
+              'twitter', 'deactivated', 'relation_partner', 'facebook',
+              'facebook_name', 'instagram', 'online_mobile', 'online_app',
+              'status_audio']
 
-head_add = []
+data_list =[]
+''' массив всех данных'''
 
 def API(token):
     """
@@ -51,7 +66,7 @@ def API(token):
 
 
 
-def user_field(pars_id):
+def user_field(pars_id, offset):
     """
 
     :param pars_id: id для парсинга данных страницы
@@ -63,17 +78,18 @@ def user_field(pars_id):
 
     user_fields = vk_api.groups.getMembers(
         group_id=pars_id,
-        offset=0,
+        offset=offset,
         count=1000,
         fields=' sex, bdate, city, country, photo_50, photo_100, photo_200_orig, photo_200, photo_400_orig, photo_max, photo_max_orig, online, online_mobile, lists, domain, has_mobile, contacts, connections, site, education, universities, schools, can_post, can_see_all_posts, can_see_audio, can_write_private_message, status, last_seen, common_count, relation, relatives')
 
 
     return user_fields
 ''' скрипт создания шапки таблицы'''
-p = user_field(id)
-n=0
+# p = user_field(id)
+# n=0
+'''
 for el in p['items']:
-    '''
+    
     print(el)
     print('_______________________________________________')
     
@@ -84,25 +100,28 @@ for el in p['items']:
             head_add.append(key_el)
             print(n, key_el, el[key_el])
             n = n + 1
-    '''
+'''
 
 
 def data_change_by_row(row, head_group):
     data_row = list()
     num = 0
     ''' обходим данные по счетчику '''
-    while num < 50:
+    while num < 51:
         """ записываем строковые данные """
         if head_group[num] in row:
 
-            if num < 11 or 12 < num < 27 or 27 < num < 38 or 40 < num < 43 or 43 < num < 49:
+            if num < 11 or 12 < num < 27 or 28 < num < 39 or 41 < num < 44 or 44 < num < 50:
                 data_row.append(row[head_group[num]])
             if 10 < num < 13:
                 data_row.append(row[head_group[num]]['title'])
             if num == 27:
-                time_el =str(row[head_group[num]]['platform'])+" "+str(datetime.datetime.fromtimestamp(row[head_group[num]]['time']))
+                time_el =row[head_group[num]]['platform']
                 data_row.append(time_el)
-            if 37 < num < 41:
+            if num==28:
+                time_el=str(datetime.date.fromtimestamp(row[head_group[num]]['time']))
+                data_row.append(time_el)
+            if 38 < num < 42:
                 str_el = ''
                 if len(row[head_group[num]]) > 0:
                     for el in row[head_group[num]]:
@@ -112,7 +131,7 @@ def data_change_by_row(row, head_group):
                     data_row.append(str_el)
                 else:
                     data_row.append('-')
-            if num == 49:
+            if num == 50:
                 str_el = ' '
                 if len(row[head_group[num]]) > 0:
                     str_el = str_el + 'true'
@@ -123,19 +142,17 @@ def data_change_by_row(row, head_group):
     return data_row
 
 def data_change(p):
-    data_list =[]
+
     for el in p['items']:
-        print('working')
         rowElement=data_change_by_row(el, head_group)
-        print('ok')
         data_list.append(rowElement)
     return data_list
 
 
 
-result=data_change(p)
-for el in result:
-    print(el)
+# result=data_change(p)
+# for el in result:
+#    print(el)
 
 
 '''
@@ -160,6 +177,30 @@ def xl(data_list):
             cell = sheet.cell(row=row, column=col)
             cell.value = inform
 
-    wb.save('test_pars\parsing_group_test.xlsx')
+    wb.save(str(id)+'\\'+str(id)+'.xlsx')
 
-run=xl(result)
+# run=xl(result)
+
+def offset_step():
+    step=0
+    while step < 10:
+        print('pars', step)
+        offset = step*1000
+        p=user_field(id, offset)
+        result=data_change(p)
+        step = step +1
+    print('write exel')
+    write_xl = xl(result)
+    print('write  txt')
+    list_id =open(str(id)+'\\'+str(id)+'_id.txt', 'w')
+    str_id =''
+    for user in result:
+        str_id = str_id + str(user[1])
+        str_id = str_id + ', '
+    list_id.write(str_id)
+    list_id.close()
+
+
+
+
+start = offset_step()
